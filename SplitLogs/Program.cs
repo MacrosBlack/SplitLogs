de441a1f9abc245fefbbdfa85961de44244354cf
MacrosBlack <>
2018-03-16 10:32:24 +0100

    Initial

--- SplitLogs/Program.cs
+++ SplitLogs/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
 
 namespace SplitLogs
 {
@@ -10,6 +9,54 @@ namespace SplitLogs
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please specify the name of a zip file containing ED Journal logfiles");
+                return;
+            }
+
+            var zipFile = args[0];
+            if( !File.Exists(zipFile))
+            {
+                Console.WriteLine($"The specified name '{zipFile}' cannot be found");
+                return;
+            }
+
+            var cmdrNameRe = new Regex("\"event\":\"LoadGame\", \"Commander\":\"([^\"]+)\"", RegexOptions.Multiline);
+            var fi = new FileInfo(zipFile);
+
+            using (var archive = ZipFile.OpenRead(zipFile))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    if (!entry.FullName.EndsWith(".log", StringComparison.OrdinalIgnoreCase))
+                    {
+                        continue;
+                    }
+
+                    var tempFile = Path.GetTempFileName();
+                    entry.ExtractToFile(tempFile, true);
+                    var content = File.ReadAllText(tempFile);
+                    var match = cmdrNameRe.Match(content);
+                    if (match.Success)
+                    {
+                        var cmdrName = match.Groups[1].Value;
+                        var cmdrNameDir = Path.Combine(fi.DirectoryName, cmdrName);
+                        if (!Directory.Exists(cmdrNameDir))
+                        {
+                            Directory.CreateDirectory(cmdrNameDir);
+                        }
+
+                        var destFileName = Path.Combine(cmdrNameDir, entry.Name + "." + cmdrName + ".log");
+                        if (!File.Exists(destFileName))
+                        {
+                            File.Copy(tempFile, destFileName);
+                        }
+
+                        File.Delete(tempFile);
+                    }
+                }
+            }
         }
     }
 }
